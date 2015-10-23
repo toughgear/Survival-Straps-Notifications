a851166aa701cca6a9d7428c871dfd1791ff91a8
Eric Patrick <erstpa@gmail.com>
2015-10-23 17:33:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Survival Straps Notifications
+
+After the provisioning of the Active Edge Gear Notifications, customer service
+reported a nearly 100% fall in customers getting in touch asking questions after
+making an order. Thus, the same must be done for Survival Straps. 
+
+
+## References
+
+- https://docs.shopify.com/manual/settings/notifications/email-variables
+- http://zurb.com/ink/docs.php
+
+
+## Changelog
+
+### October 2015
+
---
+++ snippets/button.php
@@ -0,0 +1,45 @@
+                          </td>
+                          <td class="expander"></td>
+                        </tr>
+                      </table>
+
+                    </td>
+                  </tr>
+                </table> <!-- /bodyText 1 -->
+
+
+                <table class="row buttonText"> <!-- buttonText -->
+                  <tr>
+                    <td class="wrapper last">
+
+                      <table class="twelve columns">
+                        <tr>
+                          <td class="three sub-columns center">
+                          </td>
+                          <td class="six sub-columns center">
+                            <table class="button radius">
+                              <tr>
+                                <td>
+                                  <a href="<?php echo $buttonUrl; ?>"><?php echo $buttonText; ?></a>
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                          <td class="three sub-columns last center">
+                          </td>
+                          <td class="expander"></td>
+                        </tr>
+                      </table>
+
+                    </td>
+                  </tr>
+                </table> <!-- /buttonText -->
+
+
+                <table class="row bodyText"> <!-- bodyText 2 -->
+                  <tr>
+                    <td class="wrapper last">
+
+                      <table class="twelve columns">
+                        <tr>
+                          <td class="text-pad">
